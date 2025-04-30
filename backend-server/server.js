f9131b417def730be6574b539545c04bca316af6
Hard1stf <hardikvijeta2@gmail.com>
2025-04-30 18:22:28 +0000

    Revert "code: includeing JWT and set Token  key."
    This reverts commit 06ec25afa7606036fc46c66d991820bb8b6687a3.

--- backend-server/server.js
+++ backend-server/server.js
@@ -1,15 +1,4 @@
 
 
 const express = require('express');
-const jwt = require('jsonwebtoken');
-const path = require('path');
-
-
-const app = express();
-const JWT_SECRET = "HARDIK-VIJETA";
-
-
-
-app.listen(PORT, () => {
-    console.log(`Server is ONLINE --> "http://localhost:${PORT}"\n`);
-})
+const 
