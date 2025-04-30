06ec25afa7606036fc46c66d991820bb8b6687a3
Hard1stf <hardikvijeta2@gmail.com>
2025-04-30 18:21:16 +0000

    code: includeing JWT and set Token  key.

--- backend-server/server.js
+++ backend-server/server.js
@@ -1,4 +1,15 @@
 
 
 const express = require('express');
-const 
+const jwt = require('jsonwebtoken');
+const path = require('path');
+
+
+const app = express();
+const JWT_SECRET = "HARDIK-VIJETA";
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server is ONLINE --> "http://localhost:${PORT}"\n`);
+})
